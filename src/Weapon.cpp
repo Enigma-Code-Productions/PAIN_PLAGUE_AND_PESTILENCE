21173fbc1cb84f0d728c3678b49b3eb99e40067b
Botkik-hub <stas.voronov.91@mail.ru>
2022-01-28 13:18:54 -0500

    [MOD] Weapon template

--- src/Weapon.cpp
+++ src/Weapon.cpp
@@ -1,1 +1,53 @@
 #include "Weapon.h"
+#include "TextureManager.h"
+
+
+Weapon::Weapon()
+{
+	TextureManager::Instance().load("../Assets/textures/Circle.png", "circle");
+
+	const auto size = TextureManager::Instance().getTextureSize("circle");
+	setWidth(size.x);
+	setHeight(size.y);
+	getTransform()->position = glm::vec2(100.0f, 100.0f);
+	getRigidBody()->velocity = glm::vec2(0, 0);
+	getRigidBody()->isColliding = false;
+
+	setType(NONE);
+}
+
+Weapon::~Weapon()
+= default;
+
+void Weapon::draw()
+{
+	// alias for x and y
+	const auto x = getTransform()->position.x;
+	const auto y = getTransform()->position.y;
+
+	// draw the target
+	TextureManager::Instance().draw("circle", x, y, 0, 255, true);
+}
+
+void Weapon::update()
+{
+	m_move();
+	m_checkBounds();
+}
+
+void Weapon::clean()
+{
+}
+
+void Weapon::m_move()
+{
+	getTransform()->position = getTransform()->position + getRigidBody()->velocity * 5.0f;
+}
+
+void Weapon::m_checkBounds()
+{
+}
+
+void Weapon::m_reset()
+{
+}
--- src/Weapon.h
+++ src/Weapon.h
@@ -1,7 +1,37 @@
 #pragma once
+#ifndef __WEAPON__
+#define __WEAPON__
+
 #include "DisplayObject.h"
-class Weapon :
-    public DisplayObject
+
+class Weapon : public DisplayObject
 {
+private:
+	int m_damage;
+	int m_range;
+
+public:
+	Weapon();
+	virtual ~Weapon();
+
+	// Inherited via GameObject
+	virtual void draw() override;
+	virtual void update() override;
+	virtual void clean() override;
+
+	int getDamage() const { return m_damage; }
+	int getRange() const { return m_range; }
+
+	void setDamage(int d) { m_damage = d; }
+	void setRange(int r) { m_range = r; }
+
+private:
+	void m_move();
+	void m_checkBounds();
+	void m_reset();
+
+
 };
 
+
+#endif /* defined (__TEMPLATE_OBJECT__) */
